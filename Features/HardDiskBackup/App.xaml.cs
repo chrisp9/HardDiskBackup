b238fbe97722a735d519666c131425e56264c13c
chrisp9 <chris_powell9@outlook.com>
2015-01-17 14:23:13 +0000

    Starting IOC

--- Features/HardDiskBackup/App.xaml.cs
+++ Features/HardDiskBackup/App.xaml.cs
@@ -5,6 +5,11 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Autofac;
+using Domain;
+using System.Reactive.Concurrency;
+using Services.Disk;
+using Queries;
 
 namespace HardDiskBackup
 {
@@ -13,5 +18,37 @@ namespace HardDiskBackup
     /// </summary>
     public partial class App : Application
     {
+        private ContainerBuilder _containerBuilder;
+        private IContainer _container;
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            _containerBuilder = new ContainerBuilder();
+
+            RegisterTransient<DriveInfoWrap, IDriveInfoWrap>();
+            RegisterTransient<DefaultScheduler, IScheduler>();
+
+            RegisterSingle<DriveInfoService, IDriveInfoService>();
+            RegisterSingle<DriveNotifier, IDriveNotifier>();
+            RegisterSingle<DriveInfoQuery, IDriveInfoQuery>();
+
+            _container = _containerBuilder.Build();
+
+            _container.Resolve<IDriveInfoService>();
+        }
+
+        private void RegisterSingle<T, U>()
+        {
+            _containerBuilder.RegisterType<T>()
+                .As<U>()
+                .SingleInstance();
+        }
+
+        private void RegisterTransient<T, U>()
+        {
+            _containerBuilder.RegisterType<T>()
+                .As<U>()
+                .InstancePerDependency();
+        }
     }
 }
