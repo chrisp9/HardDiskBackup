b97b97bc910319e6fe1614137be1164bbd9692f9
chrisp9 <chris_powell9@outlook.com>
2015-05-17 19:06:13 +0000

    Sketch out an implementation of RestoreBackupCommand

---
+++ Features/HardDiskBackup/Commands/RestoreBackupCommand.cs
@@ -0,0 +1,46 @@
+﻿using Domain;
+using Registrar;
+using Services.Disk.FileSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HardDiskBackup.Commands
+{
+    public interface IManageBackupCommand : ICommand { }
+
+    [Register(LifeTime.SingleInstance)]
+    public class RestoreBackupCommand
+    {
+        private IBackupFileSystem _backupFileSystem;
+        private IExistingBackupsModel _existingBackupsModel;
+
+        public RestoreBackupCommand(
+                IBackupFileSystem backupFileSystem,
+                IExistingBackupsModel existingBackupsModel)
+        {
+            _backupFileSystem = backupFileSystem;
+            _existingBackupsModel = existingBackupsModel;
+        }
+
+        public bool CanExecute(object parameter) 
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            var formattedBackup = parameter as FormattedExistingBackup;
+
+        }
+    }
+}
