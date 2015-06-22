5ded684d32c017a7e37eeb55da5670fbad8ccc0f
chrisp9 <chris_powell9@outlook.com>
2015-06-22 08:48:13 +0100

    More implementation details of RestoreBackupViewModel
    > Deal with calculating lengths of files to copy
    > Deal with telling the strategy to copy. Only the
    OriginalLocationStrategy is implemented for the time being.
    > TODO: This is a candidate for refactoring. There is some shared logic
    with BackupViewModel. For now I'm applying the rule of 3/Yagni.

--- Features/HardDiskBackup/ViewModel/RestoreBackupViewModel.cs
+++ Features/HardDiskBackup/ViewModel/RestoreBackupViewModel.cs
@@ -1,18 +1,36 @@
 ﻿using Domain;
+using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
 using Services.Disk.FileSystem;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 
 namespace HardDiskBackup.ViewModel
 {
-    public class RestoreBackupViewModel
+    public class RestoreBackupViewModel : ViewModelBase, INotifyPropertyChanged
     {
         public FormattedExistingBackup FormattedExistingBackup { get; private set; }
         public RelayCommand RestoreBackupCommand;
+
+        public long TotalBytesToCopy
+        {
+            get { return _totalBytesToCopy; }
+            set { _totalBytesToCopy = value; OnPropertyChanged(); }
+        }
+
+        public long BytesCopiedSoFar
+        {
+            get { return _bytesCopiedSoFar; }
+            set { _bytesCopiedSoFar = value; OnPropertyChanged(); }
+        }
+
+        private long _totalBytesToCopy;
+        private long _bytesCopiedSoFar;
 
         private IBackupFileSystem _backupFileSystem;
         private RestoreToOriginalLocationBackupStrategy _originalLocationStrat;
@@ -29,12 +47,21 @@ namespace HardDiskBackup.ViewModel
             RestoreBackupCommand = new RelayCommand(
                 async () =>
                 {
-                    await _originalLocationStrat.Restore(backup.ExistingBackup, (_) => { });
+                    TotalBytesToCopy = await Task.Run(() => 
+                        _backupFileSystem.CalculateTotalSize(backup.ExistingBackup.BackupDirectory));
 
+                    await _originalLocationStrat.Restore(backup.ExistingBackup, 
+                        file => { BytesCopiedSoFar += file.Length; });
                 },
                 () => true);
-
         }
 
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var handler = PropertyChangedHandler;
+
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
